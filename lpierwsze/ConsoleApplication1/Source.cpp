8246c6acd69a7b4e503f36beff8b9d25c4eaa496
Bebido <Maniek4182@interia.pl>
2016-10-13 19:58:45 +0200

    a
    a

--- lpierwsze/ConsoleApplication1/Source.cpp
+++ lpierwsze/ConsoleApplication1/Source.cpp
@@ -1,1 +1,31 @@
-#include
+//FCTRL3 - Dwie cyfry silni
+
+#include <iostream>
+using std::cin;
+using std::cout;
+
+int silnia(int);
+
+int main()
+{
+	int testy, liczba, dziesiatki, jednosci;
+	cin >> testy;
+	while (testy)
+	{
+		cin >> liczba;
+		liczba = silnia(liczba);
+		jednosci = liczba % 10;
+		dziesiatki = (liczba / 10) % 10;
+		cout << dziesiatki << " " << jednosci << "\n";
+		testy--;
+	}
+}
+
+int silnia(int n)
+{
+	int wynik = 1;
+	for (int i = 1; i <= n; ++i)
+		wynik = wynik * i;
+
+	return wynik;
+};
