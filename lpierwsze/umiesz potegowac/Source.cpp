16af5883c3180eb1ba68862991d5b21c884fee70
Bebido <Maniek4182@interia.pl>
2016-10-14 10:56:06 +0200

    q

--- lpierwsze/umiesz potegowac/Source.cpp
+++ lpierwsze/umiesz potegowac/Source.cpp
@@ -1,20 +1,18 @@
 #include <iostream>
-using std::cin;
-using std::cout;
 
 int potega_ostatnia_cyfra(int, int);
 
 int main()
 {
 	int testy, podstawa, wykladnik, wynik;
-	cin >> testy;
+	std::cin >> testy;
 	while (testy)
 	{
-		cin >> podstawa;
-		cin >> wykladnik;
+		std::cin >> podstawa;
+		std::cin >> wykladnik;
 		wynik = potega_ostatnia_cyfra(podstawa, wykladnik);
-		cout << wynik << "\n";
-		testy--;
+		std::cout << wynik << "\n";
+		--testy;
 	}
 	return 0;
 }
@@ -22,12 +20,21 @@ int main()
 int potega_ostatnia_cyfra(int podstawa, int wykladnik)
 {
 	int wynik = 1;
-	for (int i = 0; i < wykladnik; i++)
+	int wykladnik_modulo = wykladnik % 5;
+	bool petla = false;
+	for (int i = 0; i < wykladnik_modulo; ++i)
 	{
+		petla = true;
 		wynik = wynik * podstawa;
 		if (wynik > 1000)
+		{
 			wynik = wynik % 1000;
+		}
 
+	}
+	if (petla == false)
+	{
+		wynik = podstawa;
 	}
 	wynik = wynik % 10;
 	return wynik;
